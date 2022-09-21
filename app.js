0b6d872a64da7dcd5390396ced77d0b35d721480
Sujatha Mohanram <91220641+Sujatha-Mohanram@users.noreply.github.com>
2022-09-21 03:32:26 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+//IIFE - Imediately Invoked Function Expression
+(function(){
+    function start()
+    {
+        console.log('App started....');
+    }
+
+    window.addEventListener("load",start);
+})();
